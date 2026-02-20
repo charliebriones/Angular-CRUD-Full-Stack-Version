4e000ca1d6231987ace76d3b086546b3bb5e087a
Charlie Briones <charliebriones@gmail.com>
2026-02-20 16:30:12 +0800

    added footer, button style links

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, MatToolbar],
+  imports: [RouterOutlet, CommonModule, MatToolbar, MatDivider],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
